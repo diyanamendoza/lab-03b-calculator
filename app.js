0ff73a23e3c3555110ed047307c752cb7a697102
diyanamendoza <diyana.amp@pm.me>
2021-08-12 15:51:04 -0700

    Add initial html, js

--- app.js
+++ app.js
@@ -1,8 +1,48 @@
-// import functions and grab DOM elements
+// Access elements
+const numOne = document.getElementById('num1');
+const numTwo = document.getElementById('num2');
+const addButton = document.getElementById('add-button');
+const subtractButton = document.getElementById('subtract-button');
+const multiplyButton = document.getElementById('multiply-button');
+const divideButton = document.getElementById('divide-button');
+const clearButton = document.getElementById('clear-button');
+const mathResult = document.getElementById('math-result');
+const mathAction = document.getElementById('math-action');
 
-// initialize state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// function calculateAnswer () {
+//   if (addButton.clicked == true) {
+ 
+//   }
+// }
+
+addButton.addEventListener('click', () => {
+  let answer = (Number(numOne.value) + Number(numTwo.value));
+  mathResult.textContent = `= ${answer}`;
+  mathAction.textContent = "+";
+})
+
+subtractButton.addEventListener('click', () => {
+  let answer = (Number(numOne.value) - Number(numTwo.value));
+  mathResult.textContent = `= ${answer}`;
+  mathAction.textContent = "-";
+})
+
+multiplyButton.addEventListener('click', () => {
+  let answer = (Number(numOne.value) * Number(numTwo.value));
+  mathResult.textContent = `= ${answer}`;
+  mathAction.textContent = "*";
+})
+
+divideButton.addEventListener('click', () => {
+  let answer = (Number(numOne.value) / Number(numTwo.value));
+  mathResult.textContent = `= ${answer}`;
+  mathAction.textContent = "/";
+})
+
+clearButton.addEventListener('click', () => {
+  numOne.value = "";
+  numTwo.value = "";
+  mathResult.textContent = "";
+  mathAction.textContent = "";
+})
